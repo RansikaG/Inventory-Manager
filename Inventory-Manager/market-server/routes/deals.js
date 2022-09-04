5d2a4e28e812a2d24c9c24aad05e43fcbc21e6fe
Ransika Gunasekara <56599272+RansikaG@users.noreply.github.com>
2022-09-04 13:07:49 +0530

    db

---
+++ Inventory-Manager/market-server/routes/deals.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var dbAbstractionLayer = require('../public/javascripts/DBAbstractionLayer');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+  // setTimeout(() =>{
+  //   let jsonResponse ={
+  //     "handsetCards": [
+  //       { imageName:'offer1', title: '10% off', cols: 2, rows: 1 },
+  //       { imageName:'offer2',title: '5% off', cols: 2, rows: 1 },
+  //       { imageName:'offer3',title: 'random ', cols: 2, rows: 1 },
+  //       { imageName:'offer4',title: 'free', cols: 2, rows: 1 }
+  //     ],
+  //     "webCards": [
+  //         { imageName:'offer1',title: '10% off', cols: 2, rows: 1 },
+  //         { imageName:'offer2',title: '5% off', cols: 1, rows: 1 },
+  //         { imageName:'offer3',title: 'random ', cols: 1, rows: 2 },
+  //         { imageName:'offer4',title: 'free', cols: 1, rows: 1 }
+  //       ]
+  //   };
+  //   res.json(jsonResponse)
+  // }, 3000);
+dbAbstractionLayer.queryDealsCollection().then(response=>{
+  res.json(response);
+}).catch(error=>{
+  res.status(500).json({})
+});
+
+});
+
+module.exports = router;
---
+++ Inventory-Manager/src/app/home/home.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { AppService } from '../app.service';
+import { NotifierService } from '../notifier.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = [];
+  cardsForHandset = [];
+  cardsForWeb =[];
+
+  isHandset: boolean = false;
+  isHandsetObserver: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return true;
+      }
+      return false;
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver,
+    public appService: AppService,
+    private notifierService:NotifierService) { }
+
+  ngOnInit(){
+    this.isHandsetObserver.subscribe(currentObserverValue =>{
+      this.isHandset = currentObserverValue;
+      this.loadCards();
+    });
+
+    this.appService.getDeals().subscribe(
+      response => {
+        this.cardsForHandset = response.handsetCards;
+        this.cardsForWeb = response.webCards;
+        this.loadCards();
+        this.notifierService.showNotification('Todays deals loaded successfully!','OK', 'success');
+      },
+      error => {
+        this.notifierService.showNotification('There was an errorin recieving data from server!','OK', 'error');
+        // alert('There was an error in recieving data from server. Please try again later')
+      }
+    )
+  }
+
+  loadCards() {
+    this.cards = this.isHandset ? this.cardsForHandset : this.cardsForWeb;
+  }
+
+  getImage(imageName: string): string {
+    return 'url(' + 'http://localhost:3000/images/' + imageName +'.jpg' + ')';
+  }
+}
