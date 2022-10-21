893720e606ff0ee5e29af3bded9b7305aefb6af4
Ransika Gunasekara <56599272+RansikaG@users.noreply.github.com>
2022-10-21 15:48:11 +0530

    removedDbLayer

--- market-server/routes/deals.js
+++ market-server/routes/deals.js
@@ -5,29 +5,29 @@ var dbAbstractionLayer = require('../public/javascripts/DBAbstractionLayer');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  // setTimeout(() =>{
-  //   let jsonResponse ={
-  //     "handsetCards": [
-  //       { imageName:'offer1', title: '10% off', cols: 2, rows: 1 },
-  //       { imageName:'offer2',title: '5% off', cols: 2, rows: 1 },
-  //       { imageName:'offer3',title: 'random ', cols: 2, rows: 1 },
-  //       { imageName:'offer4',title: 'free', cols: 2, rows: 1 }
-  //     ],
-  //     "webCards": [
-  //         { imageName:'offer1',title: '10% off', cols: 2, rows: 1 },
-  //         { imageName:'offer2',title: '5% off', cols: 1, rows: 1 },
-  //         { imageName:'offer3',title: 'random ', cols: 1, rows: 2 },
-  //         { imageName:'offer4',title: 'free', cols: 1, rows: 1 }
-  //       ]
-  //   };
-  //   res.json(jsonResponse)
-  // }, 3000);
-dbAbstractionLayer.queryDealsCollection().then(response=>{
-  res.json(response);
-}).catch(error=>{
-  res.status(500).json({});
-  console.log(error);
-});
+  setTimeout(() =>{
+    let jsonResponse ={
+      "handsetCards": [
+        { imageName:'offer1', title: '10% off', cols: 2, rows: 1 },
+        { imageName:'offer2',title: '5% off', cols: 2, rows: 1 },
+        { imageName:'offer3',title: 'random ', cols: 2, rows: 1 },
+        { imageName:'offer4',title: 'free', cols: 2, rows: 1 }
+      ],
+      "webCards": [
+          { imageName:'offer1',title: '10% off', cols: 2, rows: 1 },
+          { imageName:'offer2',title: '5% off', cols: 1, rows: 1 },
+          { imageName:'offer3',title: 'random ', cols: 1, rows: 2 },
+          { imageName:'offer4',title: 'free', cols: 1, rows: 1 }
+        ]
+    };
+    res.json(jsonResponse)
+  }, 3000);
+// dbAbstractionLayer.queryDealsCollection().then(response=>{
+//   res.json(response);
+// }).catch(error=>{
+//   res.status(500).json({});
+//   console.log(error);
+// });
 
 });
 
